26d189d0dfd69b2db649d4a5f744bb180b24acc3
removeif <yuechao.li@mail.dealmoon.com>
2019-12-20 10:48:22 +0800

    fix gitalk generate id

--- themes/icarus/includes/helpers/layout.js
+++ themes/icarus/includes/helpers/layout.js
@@ -40,4 +40,16 @@ module.exports = function (hexo) {
         columns += hasColumn('right') ? 1 : 0;
         return columns;
     });
+
+    // 获取路劲中最后一个字符串，针对于后面调整分类和链接格式时，id不受影响
+    hexo.extend.helper.register('get_path_end_str',function (path) {
+        let pathArr = path.split("/");
+        let pathArrEndIndex = pathArr.length-1;
+        // 对于页的处理
+        if(pathArr[pathArrEndIndex] == "index.html"){
+            return (pathArr[pathArrEndIndex-1]+"/"+pathArr[pathArrEndIndex]).toLocaleLowerCase();
+        }else{
+            return pathArr[pathArrEndIndex].toLocaleLowerCase();
+        }
+    })
 }
