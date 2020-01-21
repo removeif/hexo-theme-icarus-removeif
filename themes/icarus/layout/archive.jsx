4df3846ad448155374ce605533abac7d2d21e6ff
removeif <yuechao.li@mail.dealmoon.com>
2020-01-21 10:40:04 +0800

    fix
    (cherry picked from commit 1783dfd)

--- themes/icarus/layout/archive.jsx
+++ themes/icarus/layout/archive.jsx
@@ -150,7 +150,7 @@ module.exports = class extends Component {
                     <div style="post-calendar-pre">
                         <div id="post-calendar"></div>
                     </div>
-                    <script type="text/javascript" src="/js/echarts.min.js"></script>
+                    <script type="text/javascript" src={url_for("/js/echarts.min.js")}></script>
                     <script type="text/javascript" dangerouslySetInnerHTML={{ __html: js }}></script>
                     {articleList}
                 </div>
